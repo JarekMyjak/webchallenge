3296d0c99feffd8a809fe8758ddec970ca5937ac
Jarosław <j.myjak@optimumpareto.com>
2022-12-06 21:40:23 +0100

    try catch in entries

--- backend/src/routes/api/entries.ts
+++ backend/src/routes/api/entries.ts
@@ -11,64 +11,95 @@ const { ObjectId } = require('mongodb');
 const router = Router();
 
 router.post('/challenge/:id', requireJwtAuth, async (req, res) => {
-    const userEntry = req.user as userType;
-    const user = await User.findOne({ githubId: userEntry.githubId });
+	try {
+		const userEntry = req.user as userType;
+		const user = await User.findOne({ githubId: userEntry.githubId });
 
-    const challengeId = req.params["id"]
+		const challengeId = req.params["id"]
 
-    const newEntry = await new Entry({
-        challengeId: ObjectId(challengeId),
-        userId: user._id,
-        githubUrl: req.body.githubUrl,
-        pagesUrl: req.body.pagesUrl,
-        description: req.body.description,
-    }).save();
+		const newEntry = await new Entry({
+			challengeId: ObjectId(challengeId),
+			userId: user._id,
+			githubUrl: req.body.githubUrl,
+			pagesUrl: req.body.pagesUrl,
+			description: req.body.description,
+		}).save();
+		res.send(newEntry);
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ message: error.message });
+	}
 });
 
 router.get('/challenge/:id', requireJwtAuth, async (req, res) => {
-    const userEntry = req.user as userType;
-    const user = await User.findOne({ githubId: userEntry.githubId });
-    const entries = await Entry.find({ challengeId: req.params["id"]});
-    const resEntries = entries.map(entry => entry.userJSON(user.id));
-    res.send(resEntries)
+	try {
+		const userEntry = req.user as userType;
+		const user = await User.findOne({ githubId: userEntry.githubId });
+		const entries = await Entry.find({ challengeId: req.params["id"] });
+		const resEntries = entries.map(entry => entry.userJSON(user.id));
+		res.send(resEntries)
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ message: error.message });
+	}
 });
 
 router.get('/user/:id', async (req, res) => {
-    const userEntry = req.user as userType;
-    const user = await User.findOne({ githubId: userEntry.githubId });
-    const entries = await Entry.find({ userId: req.params["id"]});
-    const resEntries = entries.map(entry => entry.userJSON(user.id));
-    res.send(resEntries)
+	try {
+		const userEntry = req.user as userType;
+		const user = await User.findOne({ githubId: userEntry.githubId });
+		const entries = await Entry.find({ userId: req.params["id"] });
+		const resEntries = entries.map(entry => entry.userJSON(user.id));
+		res.send(resEntries)
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ message: error.message });
+	}
 });
 
 router.get('/:id', async (req, res) => {
-    const entry = await Entry.findById(req.params["id"]);
-    const resEntry = entry.toJSON();
-    res.send(resEntry)
+	const entry = await Entry.findById(req.params["id"]);
+	const resEntry = entry.toJSON();
+	res.send(resEntry)
 });
 
 router.post('/:id/like', requireJwtAuth, async (req, res) => {
-    const userEntry = req.user as userType;
-    const user = await User.findOne({ githubId: userEntry.githubId });
-    await Entry.findByIdAndUpdate(req.params.id, {$addToSet: {likedUserIds: user.id}})
+	try {
+		const userEntry = req.user as userType;
+		const user = await User.findOne({ githubId: userEntry.githubId });
+		await Entry.findByIdAndUpdate(req.params.id, { $addToSet: { likedUserIds: user.id } })
 
-    res.sendStatus(200);
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ message: error.message });
+	}
 });
 
 router.post('/:id/dislike', requireJwtAuth, async (req, res) => {
-    const userEntry = req.user as userType;
-    const user = await User.findOne({ githubId: userEntry.githubId });
-    const entry = await Entry.findByIdAndUpdate(req.params.id, {$pull: {likedUserIds: user.id}})
+	try {
+		const userEntry = req.user as userType;
+		const user = await User.findOne({ githubId: userEntry.githubId });
+		const entry = await Entry.findByIdAndUpdate(req.params.id, { $pull: { likedUserIds: user.id } })
 
-    res.sendStatus(200);
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ message: error.message });
+	}
 });
 
 router.post('/:id/comment', requireJwtAuth, async (req, res) => {
-    const userEntry = req.user as userType;
-    const user = await User.findOne({ githubId: userEntry.githubId });
-    const newComment = {userId: user.id, userName: user.username, content: req.body.content}
-    const entry = await Entry.findByIdAndUpdate(req.params.id, {$push: {comments: newComment}})
-    res.sendStatus(200);
+	try {
+		const userEntry = req.user as userType;
+		const user = await User.findOne({ githubId: userEntry.githubId });
+		const newComment = { userId: user.id, userName: user.username, content: req.body.content }
+		const entry = await Entry.findByIdAndUpdate(req.params.id, { $push: { comments: newComment } })
+		res.sendStatus(200);
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ message: error.message });
+	}
 });
 
 export default router;
